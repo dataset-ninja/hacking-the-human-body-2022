685687623f62d343e0587a927d9af2993657aa3c
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-03 00:12:23 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -57,6 +57,7 @@ REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"Alternative Kagg
 
 CITATION_URL: Optional[str] = "https://www.kaggle.com/competitions/hubmap-organ-segmentation/overview/citation"
 AUTHORS: Optional[List[str]] = ["Addison Howard", "Almost Sohier", "Cecilia Lindskog", "Emma Lundberg", "Katy Borner", "Leah Godwin", "Shriya Dane", "Sohier Dane", "Trang Le", "Yashvardhan Jain"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://hubmapconsortium.org/contact-form/"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
@@ -110,6 +111,7 @@ def get_settings():
     settings["repository"] = REPOSITORY
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS    
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
